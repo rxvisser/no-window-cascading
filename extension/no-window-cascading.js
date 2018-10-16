4722b8b1d98540c443420e00a2d2c8cfb22c95cd
rxvisser <rxvisser@gmail.com>
2018-10-16 21:33:07 +0200

    Make mitigation relative to position

--- extension/no-window-cascading.js
+++ extension/no-window-cascading.js
@@ -1,3 +1,8 @@
-chrome.windows.onCreated.addListener(function(new_window) {
-  chrome.windows.update(new_window.id, {left:0, top:0});
+chrome.windows.onCreated.addListener(function(w) {
+  // Cascading offset in pixels.
+  var offset = 10
+
+  // Filter out popups, devtools, minimized, maximized, fullscreen.
+  if (w.type == "normal" && w.state == "normal")
+    chrome.windows.update(w.id, {left: w.left - offset, top: w.top - offset});
 });
